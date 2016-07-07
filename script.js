80f5523ba8b959c7864aca722748fd6678ce9b6f
Erika Bergman <erika.a.bergman@gmail.com>
2016-07-07 11:42:50 -0700

    code_challenge

---
+++ script.js
@@ -0,0 +1,30 @@
+var event;
+var temp;
+
+// var event='casual','semi formal','formal';
+var temp=60;
+
+if(temp>70){
+	console.log('no jacket');
+}
+else if(temp<=70 && temp>=54){
+	console.log('wear jacket');
+}
+else{
+	console.log('wear a coat');
+}
+
+var event='formal';
+
+if(event==='formal'){
+	console.log('wear a suit');
+}
+else if(event==='semi formal'){
+	console.log('wear a polo');
+}
+else{
+	console.log('wear something comfy');
+}
+
+var outfit= output event;
+console.log(outfit);
